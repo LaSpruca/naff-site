3ccd1c200e38a4b0b7c1bd727ec9737279a14279
Nathan Hare <me@laspruca.nz>
2022-12-20 01:49:32 +1300

    Use 0.0.0.0 for api

--- backend/src/main.rs
+++ backend/src/main.rs
@@ -50,7 +50,7 @@ async fn start() -> Result<(), Error> {
             .service(api::api())
             .service(auth())
     })
-    .bind("127.0.0.1:8080")
+    .bind("0.0.0.0:8080")
     .map_err(|ex| Error::ServerStartError(ex))?
     .run()
     .await
